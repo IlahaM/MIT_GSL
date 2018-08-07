28911385d9b7f4372963e32b069c983236eb5a85
IlahaM <34169866+IlahaM@users.noreply.github.com>
2018-08-07 10:02:42 +0400

    Update sign.php

--- sign.php
+++ sign.php
@@ -4,9 +4,10 @@
 <body>
 <?php
 
-if(isset($_POST['submit'])){
+if(isset($_POST['submitSt']) || isset($_POST['submitT'])){
 
 echo "submit is clicked";
+echo "</br>";
 
 $con = mysqli_connect("localhost", "Ilaha", "password");
 
@@ -30,33 +31,18 @@ values('$_POST[firstName]',
 if ($con->query($sql) === TRUE) {
     $last_id = $con->insert_id;
     echo "New record created successfully. Last inserted ID is: " . $last_id;
+	echo "</br>";
 } 
 
-$sql2 = "insert into Details (id, verified, p_picture, p_video, expertise, education,
+$sql2 = "insert into Details (id, verified, gender, p_picture, p_video, expertise, education,
 experience, bio, methodology, phone, location, rating_count, rating_sum, price_hour)
-values ('$last_id', '0', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', '1', '1', '1' )";
+values ('$last_id', '0', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)";
 
+$sql3 = "insert into image_table (id, folder, upload_image)
+values ('$last_id', NULL, NULL)";
 
-//$result = mysqli_query($con, $sql);
 $result2 = mysqli_query($con, $sql2);
-
-/*$sql = "create table Details(
-id integer not null,
-verified boolean,
-p_picture text,
-p_video text,
-expertise text,
-education text,
-experience text,
-bio text,
-methodology varchar(255),
-phone varchar(255),
-location text,
-rating_count integer,
-rating_sum integer,
-price_hour integer,
-foreign key (id) references User(id)
-)";*/
+$result3 = mysqli_query($con, $sql3);
 
 mysqli_close($con);
 }
